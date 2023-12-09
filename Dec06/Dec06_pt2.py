a52405d6aa758d01bd45ef22844345c5599488cb
unknown <parappa77@hotmail.com>
2023-12-09 11:27:27 -0500

    misc files added

--- Dec06/Dec06_pt2.py
+++ Dec06/Dec06_pt2.py
@@ -10,6 +10,8 @@ data = read_file(path)
 def clean_data(unclean_data):
     cleaned_time = []
     cleaned_distance = []
+    new_time_values = ''
+    new_distance_values = ''
     for line in unclean_data:
         if "Time" in line:
             remove_heading = line.strip().split('        ')
@@ -17,35 +19,37 @@ def clean_data(unclean_data):
         if "Distance" in line:
             remove_heading = line.strip().split('   ')
             distance_values = remove_heading[1:]
-    return(time_values, distance_values)
+    for time in time_values:
+        new_time_values += time
+    for distance in distance_values:
+        new_distance_values += distance
+    return(new_time_values, new_distance_values)
 
 cleaned_data = clean_data(data)
 Time_of_race = cleaned_data[0]
 Distance_needed = cleaned_data[1]
 
-def race_attempt_winners(race_results, distance_needed_to_win, race_number): 
+def race_attempt_winners(race_results, distance_needed_to_win): 
     counter = 0
     for num in race_results:
-        if num > int(distance_needed_to_win[race_number]):
+        if num > int(distance_needed_to_win):
             counter += 1
     return(counter)
 
 
 def race_options(total_time, distance_needed_to_win):
     result_possibilities = []
-    races = range(len(total_time))
-    for race in races:
-        distance_values = []
-        race_time = int(total_time[race])
-        attempt_range = range(race_time)
-        for attempt in attempt_range:
-            speed = int(attempt)
-            time_held = int(attempt)
-            distance_travelled = speed * (race_time - time_held)
-            distance_values.append(distance_travelled)
-            #print(f'For race {race}, we hold for {time_held}ms, the boat goes {speed} per ms and travels {distance_travelled}mm.')
-        count_of_winners = race_attempt_winners(distance_values, distance_needed_to_win, race)
-        result_possibilities.append(race_attempt_winners(distance_values, distance_needed_to_win, race))
+    distance_values = []
+    race_time = int(total_time)
+    attempt_range = range(race_time)
+    for attempt in attempt_range:
+        speed = int(attempt)
+        time_held = int(attempt)
+        distance_travelled = speed * (race_time - time_held)
+        distance_values.append(distance_travelled)
+        #print(f'For race {race}, we hold for {time_held}ms, the boat goes {speed} per ms and travels {distance_travelled}mm.')
+    count_of_winners = race_attempt_winners(distance_values, distance_needed_to_win)
+    result_possibilities.append(race_attempt_winners(distance_values, distance_needed_to_win))
     return(result_possibilities)
 
 results = race_options(Time_of_race, Distance_needed)
